1852d4056e0aac402ac7ab6c81edfb2cf669078d
Alex Clayton <alexander.clayton.email@gmail.com>
2023-04-17 12:17:34 -0500

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React, { FC } from 'react';
+
+
+const App: FC = () => {
+
+
+  return (
+    <div className="App">
+      <div>
+
+      </div>
+      <div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default App
